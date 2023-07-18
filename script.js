9172105dff5846b010b80e1f4d3103a5280c7460
ValladaresRolo <99679490+ValladaresRolo@users.noreply.github.com>
2023-07-18 17:51:39 -0400

    el js base

--- script.js
+++ script.js
@@ -7,20 +7,109 @@ const phoneInput = document.getElementById('phone-input'); // id telefono
 const nameList = document.getElementById('name-list'); // lista
 
 
+
+
 // Array para almacenar los nombres
 let datas = [];
 
 // Función para agregar un nombre a la lista
-function adddata (event) { 
-    event.preventDefault(); // Evita que el formulario se envíe
-    const name = nameInput.value.trim(); // trim() funcion propia de js para que no se guarde espacios vacios antes o despues del valor ingresado
-    // compruebo que la constante name no este vacia
-    if (name === '') {
-      alert('Por favor, ingrese un nombre válido.'); // envio un alerta para que ingrese valor respectivo
-      return;
-    }
-  
-    datas.push(data); // agrego el valor ingresado al array names de la constante name
-    
-    nameInput.value = ''; // vacio el valor de la constante nameImput
+function addName(event) {
+  event.preventDefault();
+  const name = nameInput.value.trim();
+  const lastName = lastNameInput.value.trim();
+  const email = emailInput.value.trim();
+  const phone = phoneInput.value.trim();
+
+  if (name === '') {
+    alert('Por favor, ingrese un nombre válido.');
+    return;
   }
+  if (lastName === '') {
+    alert('Por favor, ingrese un Apellido válido.');
+    return;
+  }
+  if (email === '') {
+    alert('Por favor, ingrese un email válido.');
+    return;
+  }
+  if (phone === '') {
+    alert('Por favor, ingrese un telefono válido.');
+    return;
+  }
+
+  datas.push({ name, lastName, email, phone });
+
+  savedatasToLocalStorage();
+  renderdatas();
+  nameInput.value = '';
+  lastNameInput.value = '';
+  emailInput.value = '';
+  phoneInput.value = '';
+}
+
+// Función para mostrar la lista de nombres en el DOM
+function renderdatas() {
+  nameList.innerHTML = '';
+
+  datas.forEach((data, index) => {
+    const li = document.createElement('li');
+    li.textContent = `${data.name} ${data.lastName} ${data.email} ${data.phone}`;
+    li.innerHTML += `<button onclick="editName(${index})">Editar</button> <button onclick="deleteName(${index})">Eliminar</button>`;
+    nameList.appendChild(li);
+  });
+}
+
+// Función para editar un nombre en la lista
+function editName(index) {
+  const newName = prompt('Ingrese un nuevo nombre:', datas[index].name);
+  const newLastName = prompt('Ingrese un nuevo apellido:', datas[index].lastName);
+  const newEmail = prompt('Ingrese un nuevo email:', datas[index].email);
+  const newPhone = prompt('Ingrese un nuevo teléfono:', datas[index].phone);
+
+  if (
+    newName === null || newName.trim() === '' ||
+    newLastName === null || newLastName.trim() === '' ||
+    newEmail === null || newEmail.trim() === '' ||
+    newPhone === null || newPhone.trim() === ''
+  ) {
+    return;
+  }
+
+  datas[index].name = newName.trim();
+  datas[index].lastName = newLastName.trim();
+  datas[index].email = newEmail.trim();
+  datas[index].phone = newPhone.trim();
+  savedatasToLocalStorage();
+  renderdatas();
+}
+
+
+
+// Función para eliminar un nombre de la lista
+function deleteName(index) {
+  if (confirm('¿Está seguro de eliminar este nombre?')) {
+    datas.splice(index, 1);
+    savedatasToLocalStorage();
+    renderdatas();
+  }
+}
+
+// Función para guardar los nombres en el almacenamiento local
+function savedatasToLocalStorage() {
+  localStorage.setItem('datas', JSON.stringify(datas));
+}
+
+// Función para cargar los nombres desde el almacenamiento local
+function loaddatasFromLocalStorage() {
+  const storeddatas = localStorage.getItem('datas');
+  if (storeddatas) {
+    datas = JSON.parse(storeddatas);
+  }
+}
+
+// Cargar los nombres desde el almacenamiento local al inicio
+loaddatasFromLocalStorage();
+renderdatas();
+
+// Agregar el evento de envío al formulario
+nameForm.addEventListener('submit', addName);
